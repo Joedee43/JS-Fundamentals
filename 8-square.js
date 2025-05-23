f8e29aae6038c2782b4f67fc8ef6be97e2eff871
joe dee <joedee4311@gmail.com>
2025-05-23 22:59:05 +0300

    added 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,8 +2,13 @@ const arg = process.argv.slice(2);
 const val = arg[0];
 const num = Number(val);
 if(!isNaN(num)){
-for(let i=0; i<num;i++)
-  console.log("X".repeat(num))
+for (let i = 0; i < num; i++) {
+        let row = '';
+        for (let j = 0; j < num; j++) {
+            row += 'X';
+        }
+        console.log(row);
+    }
 }
 
 else{
