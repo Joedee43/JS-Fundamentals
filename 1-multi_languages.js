84d8777521ec42ad6e0a61d0634c6c20c537ec77
joe dee <joedee4311@gmail.com>
2025-05-23 19:46:44 +0300

    added 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,10 @@
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+const firstLine ="C is fun"
+const secondLine = "Python is cool"
+const thirdLine = "JavaScript is amazing"
+
+console.log(firstLine)
+console.log(secondLine)
+console.log(thirdLine)
