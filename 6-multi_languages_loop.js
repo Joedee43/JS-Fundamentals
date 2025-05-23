e3e5524c87544930e22c8ee0bc348b545c4c11aa
joe dee <joedee4311@gmail.com>
2025-05-23 21:56:44 +0300

    added 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,2 @@
 const array = ["C is fun","Python is cool","JavaScript is amazing"]
-for(let i = 0; i < array.length;i++){
-    console.log(array[i])
-}
+array.forEach(element=>console.log(element))
