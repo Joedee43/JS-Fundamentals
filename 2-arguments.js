98cf68e5172e9320a217e8a1ba6e1e1ff7098707
joe dee <joedee4311@gmail.com>
2025-05-23 20:19:51 +0300

    updated 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const arg = process.argv.slice(2);
 if(arg.length === 0){
     console.log('No argument')
 }else if(arg.length === 1){
-    console.log('Found argument')
+    console.log('Argument found')
 }else{
-    console.log('Found arguments')
+    console.log('Arguments found')
 }
