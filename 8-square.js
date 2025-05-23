5648e09595bfa6e8285c1696c8e57782a36d24cb
joe dee <joedee4311@gmail.com>
2025-05-23 22:55:08 +0300

    added 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,11 +2,10 @@ const arg = process.argv.slice(2);
 const val = arg[0];
 const num = Number(val);
 if(!isNaN(num)){
-let i=0;
-while(i<num){
-    console.log("X".repeat(num))
-    i++
-}}
+for(let i=0; i<num;i++)
+  console.log("X".repeat(num))
+}
+
 else{
     console.log("Missing size")
 }
