5e9b595a744225d3ae6599a739a8d20d7d81d055
joe dee <joedee4311@gmail.com>
2025-05-23 20:43:18 +0300

    added 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,7 +2,5 @@ const arg = process.argv.slice(2)
 if(!arg[0]){
     console.log("No argument")
 }else{
-   arg.forEach(element => {
-    console.log(element)
-   });
+   for (const argument of arg) console.log(argument);
 }
