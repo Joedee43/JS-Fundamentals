fb1ab95e611263fb029808270610cbe6370b9f6a
joe dee <joedee4311@gmail.com>
2025-05-23 22:11:12 +0300

    added 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,9 +3,13 @@ const arg = process.argv.slice(2);
 const val = arg[0];
 const num = Number(val);
 if(!isNaN(num)){
-    for(let i=0;i<num;i++){
-        console.log("C is fun")
+    let i=0
+    while (i<num) {
+        console.log("C is fun");
+        i++;
     }
+        
+    
 }else{
     console.log("Missing number of occurrences")
 }
